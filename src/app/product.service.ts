e0c67062229333485aa608e72e412440eb224a1e
Mary Tran <marytran@cisco.com>
2019-05-08 16:09:37 -0500

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,12 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map'
+
 import {getResponseURL} from '@angular/http/src/http_utils';
+
+import { Album } from './album';
+
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -12,8 +17,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
-    response.json());
+    <Album>response.json());
   }
 }
